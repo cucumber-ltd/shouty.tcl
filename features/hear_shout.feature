c32e3749b7535adf5be47f49e0c398b9eb63d26c
Matt Wynne <matt@mattwynne.net>
2015-04-22 09:27:48 +0100

    Sketches of tcl solulution done during training

--- features/hear_shout.feature
+++ features/hear_shout.feature
@@ -1,7 +1,11 @@
 Feature: Hear shout
 
-  Your description goes here
+  Scenario: Receive a message within a given range
 
-  Scenario: TODO
+    Rule - The boundary of the range is within the range
+
     Given the range is 1000m
-
+    And Bob is at 0m
+    And Samantha is at 1000m
+    When Bob sends a message "hello"
+    Then Samantha should hear "hello"
--- features/step_definitions/steps.tcl
+++ features/step_definitions/steps.tcl
@@ -1,4 +1,20 @@
 # here's an example of how to match a step in tcl:
 Given {^the range is (\d+)m$} {range} {
-  error "Implement me! (range is $range)"
+  global current_range
+  set current_range $range
+}
+
+Given {^Samantha is 1000m from Bob$} {
+  global bob
+  global samantha
+  set bob [create_user "Bob" 0]
+  set samantha [create_user "Samantha" 1000]
+}
+
+When {^Bob sends a message "hello"$} {
+  error "TODO"
+}
+
+Then {^Samantha should hear "hello"$} {
+  error "TODO"
 }
